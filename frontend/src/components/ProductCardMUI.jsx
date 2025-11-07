4bb9e74472f1657661802c4c1c9b0d8dda94eebf
Rushi2410 <rushikeshandhale1010@gmail.com>
2025-11-07 17:31:34 +0530

    Render backend

--- frontend/src/components/ProductCardMUI.jsx
+++ frontend/src/components/ProductCardMUI.jsx
@@ -2,15 +2,17 @@ import React from "react";
 import { Card, CardContent, Typography, CardActions, Button, Box } from "@mui/material";
 
 export default function ProductCardMUI({ product, onEdit, onDelete, showActions = false }) {
+    const colors = ["#E3F2FD", "#FCE4EC", "#E8F5E9", "#FFF3E0", "#F3E5F5"];
+  const randomColor = colors[Math.floor(Math.random() * colors.length)];
   return (
-    <Card variant="outlined" sx={{ height: "100%", display: "flex", flexDirection: "column" }}>
+    <Card variant="outlined" sx={{ height: "100%", display: "flex", flexDirection: "column", backgroundColor: randomColor }}>
       <CardContent sx={{ flexGrow: 1 }}>
         <Typography variant="h6">{product.name}</Typography>
         <Typography variant="body2" color="text.secondary" sx={{ mt: 1 }}>{product.description}</Typography>
       </CardContent>
 
-      <Box sx={{ px: 2, pb: 2, display: "flex", justifyContent: "space-between", alignItems: "center" }}>
-        <Typography variant="subtitle1" color="primary">₹{product.price}</Typography>
+      <Box sx={{ px: 2, gap:2,pb: 2, display: "flex", justifyContent: "space-between", alignItems: "center" }} >
+        <Typography variant="subtitle1" color="primary">₹ {product.price}</Typography>
         <Typography variant="caption">By: {product.user?.name || "Unknown"}</Typography>
       </Box>
 
